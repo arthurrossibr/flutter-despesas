6dc84ccb36982ec205ecbe96c7b509c140bdcc53
arthurrossicardoso <arthurrossi15@gmail.com>
2022-06-09 23:02:13 -0300

    Installing INTL package for date

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './models/transaction.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -80,7 +81,7 @@ class MyHomePage extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date),
                             style: const TextStyle(
                               color: Colors.grey,
                             ),
