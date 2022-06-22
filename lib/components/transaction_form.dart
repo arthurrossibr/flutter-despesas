a6ec12f23fcabef76b9857af8cf74c8936fe059a
arthurrossicardoso <arthurrossi15@gmail.com>
2022-06-21 23:30:03 -0300

    Adding adaptative date picker component

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:intl/intl.dart';
 import 'package:projeto_despesas/components/adaptative_button.dart';
 
-import 'adaptative_textfield.dart';
+import 'adaptative_date_picker.dart';
+import 'adaptative_text_field.dart';
 
 class TransactionForm extends StatefulWidget {
   const TransactionForm(this.onSubmit, {Key? key}) : super(key: key);
@@ -28,27 +28,6 @@ class _TransactionFormState extends State<TransactionForm> {
     }
 
     widget.onSubmit(title, value, _selectedDate!);
-  }
-
-  _showDatePicker() {
-    showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2022),
-      lastDate: DateTime.now(),
-    ).then(
-      (pickedDate) {
-        if (pickedDate == null) {
-          return;
-        }
-
-        setState(
-          () {
-            _selectedDate = pickedDate;
-          },
-        );
-      },
-    );
   }
 
   @override
@@ -80,28 +59,13 @@ class _TransactionFormState extends State<TransactionForm> {
                       const TextInputType.numberWithOptions(decimal: true),
                   onSubmitted: (_) => _submitForm(),
                 ),
-                SizedBox(
-                  height: 70,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Text(_selectedDate == null
-                            ? 'Nenhuma data selecionada!'
-                            : DateFormat('dd MMMM yyyy', 'pt_BR')
-                                .format(_selectedDate!)),
-                      ),
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          textStyle: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                          primary: Theme.of(context).colorScheme.primary,
-                        ),
-                        onPressed: _showDatePicker,
-                        child: const Text('Selecionar data!'),
-                      ),
-                    ],
-                  ),
+                AdaptativeDatePicker(
+                  selectedDate: _selectedDate,
+                  onDateChanged: (newDate) {
+                    setState(() {
+                      _selectedDate = newDate;
+                    });
+                  },
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
